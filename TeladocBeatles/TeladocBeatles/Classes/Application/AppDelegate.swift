d102cbb8d67ad25520b5d92b802fe45311bf3a04
Konstantin Ionin <int4@me.com>
2023-10-14 08:59:05 +0200

    Project created and cleaned up

---
+++ TeladocBeatles/TeladocBeatles/Classes/Application/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  TeladocBeatles
+//
+//  Created by Konstantin Ionin on 14.10.2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    
+    self.window = UIWindow(frame: UIScreen.main.bounds)
+    
+    let viewController = ViewController(nibName: nil, bundle: nil)
+    self.window?.rootViewController = viewController
+    
+    self.window?.makeKeyAndVisible()
+    
+    return true
+  }
+
+}
+
---
+++ TeladocBeatles/TeladocBeatlesUITests/TeladocBeatlesUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TeladocBeatlesUITestsLaunchTests.swift
+//  TeladocBeatlesUITests
+//
+//  Created by Konstantin Ionin on 14.10.2023.
+//
+
+import XCTest
+
+final class TeladocBeatlesUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
